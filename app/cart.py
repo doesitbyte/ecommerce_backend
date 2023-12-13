6009bd5555cff76501554ecf729e373f4dadfef8
Kaustubh Sharma <kaustubhsharma999@gmail.com>
2023-12-13 17:29:44 +0530

    Added checkout API for cart

--- app/cart.py
+++ app/cart.py
@@ -1,8 +1,14 @@
 from flask_restful import Resource, reqparse
-from flask import jsonify
+from flask import jsonify, make_response, Blueprint, request
 from app import datastore
+from app.shared_data import *
+from app.coupon import validateCoupon
 
-cart = datastore["cart"]
+checkout_bp = Blueprint('checkout', __name__)
+
+cart: Cart = datastore["cart"]
+orders: OrderHistory = datastore["orders"]
+coupons: CouponManager = datastore["coupons"]
 
 class CartResource(Resource):
     def __init__(self):
@@ -21,7 +27,7 @@ class CartResource(Resource):
         # Add item to the cart
         cart.add_item(item_id, quantity)
 
-        return jsonify({'message': 'Item added to cart'})
+        return make_response(jsonify({'message': 'Item added to cart'}), 200)
     
     def delete(self):
         """Endpoint to remove items from the shopping cart."""
@@ -33,9 +39,28 @@ class CartResource(Resource):
         # Remove item from the cart
         cart.remove_item(item_id, quantity)
 
-        return jsonify({'message': 'Item removed from cart'})
+        return make_response(jsonify({'message': 'Item removed from cart'}), 200)
     
     def get(self):
         """Endpoint to get current shopping cart"""
 
-        return jsonify(cart.to_dict())
+        return make_response(jsonify(cart.to_dict()), 200)
+
+@checkout_bp.route('/cart/checkout', methods=['POST'])
+def checkout_cart():
+    """Endpoint to checkout the shopping cart."""
+    
+    data = request.get_json()
+    if 'coupon_code' in data:
+        if validateCoupon(data["coupon_code"]):
+            coupon = coupons.coupon_codes[data["coupon_code"]]
+            orders.checkout_cart(cart.to_dict(), coupon)
+            coupons.claim_coupon(data["coupon_code"])
+            cart.checkout_cart()
+            return make_response(jsonify({"message": "Checkout out successfully"}), 200)
+        else:
+            return make_response(jsonify({"message": "Checkout failed. Invalid coupon."}), 400)
+    else:
+        orders.checkout_cart(cart.to_dict(), None)
+        cart.checkout_cart()
+        return make_response(jsonify({"message": "Checkout out successfully"}), 200)
